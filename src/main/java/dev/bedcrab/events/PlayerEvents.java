44967d97188c9d11d87e8b3a20ae6be819abb402
CultistOfTheEnd <nhelwig99@gmail.com>
2023-05-10 16:36:31 +0200

    Update PlayerEvents.java

--- src/main/java/dev/bedcrab/events/PlayerEvents.java
+++ src/main/java/dev/bedcrab/events/PlayerEvents.java
@@ -7,11 +7,11 @@ public class PlayerEvents {
      * Called when a player joins
      */
     public void onJoin(PlayerLoginEvent event) {
-        final Player player = e.getPlayer();
+        final Player player = event.getPlayer();
         InstanceContainer instance = MinestomServer.getInstance().instanceContainer;
         instance.setChunkLoader(new AnvilLoader(PlayerEvents.class.getClassLoader().getResource((String) MinestomServer.configuration.get("worlds-directory") + "/world").getPath()));
 
-        p.setRespawnPoint(new Pos(0, 70, 0)); // Set the player spawn location
+        player.setRespawnPoint(new Pos(0, 70, 0)); // Set the player spawn location
         event.setSpawningInstance(instance);
     }
 }
